45598c8159e47501beecc6d1cf2c3a2014603edf
ellie <gyeongjin.hong@dailyh.org>
2023-06-21 13:13:31 +0900

    + 네비바 새로운 폴더 생성

---
+++ react-nav/src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import Navbar from './components/Navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+    </div>
+  );
+}
+export default App;
