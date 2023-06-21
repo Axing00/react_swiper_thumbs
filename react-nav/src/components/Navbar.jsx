7cd6ab0dbe9094e4f229707e308f9eb26b135cb6
ellie <gyeongjin.hong@dailyh.org>
2023-06-21 13:09:56 +0900

    + 리엑트 네비바 폴더 생성

---
+++ react-nav/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () =>{
+  return (
+    <div className="sidnav">
+      <ul>
+        <li>
+          <a href="#none">Home</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+export default Navbar
